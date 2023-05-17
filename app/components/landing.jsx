297edb5a1248df8efada30d3b51bff0d3064d1c0
Connor White <connorwhitepdx@gmail.com>
2023-05-17 15:01:36 -0700

    animated

--- app/components/landing.jsx
+++ app/components/landing.jsx
@@ -1,11 +1,37 @@
+import React, { useRef, useEffect } from 'react';
+import { gsap } from 'gsap';
+
 export default function Landing() {
+
+    const landingRef = useRef();
+
+    useEffect(() => {
+        let ctx = gsap.context(() => {
+            let tl = gsap.timeline({});
+                tl.from('.imageContainer', {
+                    duration: 2,
+                    opacity: 0,
+                    ease: 'power3.inOut',
+                });
+                tl.from('.headerCopy', {
+                    duration: 1,
+                    opacity: 0,
+                    y: 100,
+                    ease: 'power3.inOut',
+                });
+        }, landingRef.current);
+        return () => ctx.revert();
+    }, [])
+
     return (
-        <div className='home'>
+        <div className='home' ref={landingRef}>
             <div className="imageContainer">
                 <img src='./images/P-30.jpeg' alt='home' className='homeImage' />
             </div>
             <div className='header'>
-                Sq Ft magazine tells stories from the heart and outskirts of the world of art and design. We shine a light on artists and art lovers, and dissect how they shape and are shaped by the spaces they occupy.
+                <h1 className='headerCopy'>
+                    Sq Ft magazine tells stories from the heart and outskirts of the world of art and design. We shine a light on artists and art lovers, and dissect how they shape and are shaped by the spaces they occupy.
+                </h1>
             </div>
         </div>
     )
--- app/components/menu.jsx
+++ app/components/menu.jsx
@@ -1,6 +1,30 @@
+import React, { useRef, useEffect } from "react";
+import { gsap } from "gsap";
+
 export default function Menu() {
+
+    const menuRef = useRef();
+
+    useEffect(() => {
+        let ctx = gsap.context(() => {
+            let tl = gsap.timeline({});
+                tl.from('.logoBox', {
+                    duration: 1,
+                    opacity: 0,
+                    ease: 'power3.inOut',
+                });
+                tl.from('.navItem', {
+                    duration: 1,
+                    opacity: 0,
+                    y: -100,
+                    ease: 'power3.inOut',
+                });
+        }, menuRef.current);
+        return () => ctx.revert();
+    }, []);
+
     return (
-        <div className='menu'>
+        <div className='menu' ref={menuRef}>
             <div className='logoBox'><img className='logo' src='./images/logo-full-black.svg' alt='sqft logo'></img></div>
             <div className='nav'>
                 <div className='navItem'><a className='navLink'>Info</a></div>
