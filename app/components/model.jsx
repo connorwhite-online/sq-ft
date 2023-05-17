dedff0f0b5ae11b53312b7cd9b53746b1ef4984c
Connor White <connorwhitepdx@gmail.com>
2023-05-17 11:45:55 -0700

    SQFT mag model loaded and now responding to mobile and desktop conditions

--- app/components/model.jsx
+++ app/components/model.jsx
@@ -1,7 +1,9 @@
 import React, { useRef, useEffect } from 'react';
 import { useFrame } from '@react-three/fiber';
+import { useGLTF } from "@react-three/drei";
 
-export default function Model({ autoRotate }) {
+export default function Model(props) {
+  const { nodes, materials } = useGLTF("/sqft-01.glb");
   const groupRef = useRef();
 
   // Rotate the model based on mouse movement
@@ -18,7 +20,7 @@ export default function Model({ autoRotate }) {
 
   // Auto-rotate the model
   useFrame(({ clock }) => {
-    if (autoRotate) {
+    if (window.innerWidth <= 768) {
       const elapsedTime = clock.getElapsedTime();
       groupRef.current.rotation.y = elapsedTime * 0.1; // Adjust the rotation speed as needed
     }
@@ -34,11 +36,43 @@ export default function Model({ autoRotate }) {
   }, []);
 
   return (
-    <group ref={groupRef}>
-        <mesh >
-            <boxBufferGeometry attach='geometry' args={[2, 3, .25]}/>
-            <meshStandardMaterial attach='material' color='turquoise' />
-        </mesh>
+    <group {...props} dispose={null} scale={.08} ref={groupRef}>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Pages.geometry}
+        material={materials.Pages}
+        position={[0.36, 0, 0.87]}
+        scale={0.1}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Spine_Cover.geometry}
+        material={materials.Spine}
+        position={[-22.52, 0, -0.29]}
+        rotation={[Math.PI, 0, Math.PI]}
+        scale={[0.1, 29.7, 1.27]}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Back_Cover.geometry}
+        material={materials.Back}
+        position={[0.36, 0, -1.55]}
+        rotation={[Math.PI, 0, Math.PI]}
+        scale={[22.86, 29.7, 0.1]}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Front_Cover.geometry}
+        material={materials.Front}
+        position={[0.36, 0, 0.97]}
+        scale={[22.86, 29.7, 0.1]}
+      />
     </group>
   );
 };
+
+useGLTF.preload("/sqft-01.glb");
