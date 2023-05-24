8105b2fd4da285b481b9d9d8288d4497ec93feb7
Connor White <connorwhitepdx@gmail.com>
2023-05-24 14:12:38 -0700

    experimental loader

---
+++ app/components/loading.jsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+    return (
+        <div className='loading' style={{width: '100vw', height: '100vh', display: 'flex', textAlign: 'center'}}>
+            Loading...
+        </div>
+    )
+}
