6884e58b2e2c38b88cb9e6ef9201b5e0011253a5
Connor White <connorwhitepdx@gmail.com>
2023-06-05 16:17:39 -0700

    maath deleted

--- app/components/model.jsx
+++ app/components/model.jsx
@@ -1,7 +1,6 @@
 import React, { useRef, useEffect } from 'react';
 import { useFrame } from '@react-three/fiber';
 import { useGLTF } from "@react-three/drei";
-import { dampE } from "maath/easing";
 
 export default function Model(props) {
   const { nodes, materials } = useGLTF("/sqft-01.glb");
